af76aec3ddc31560ea098be85ec69d8362d93f9e
Shanto Islam <ishanto412@gmail.com>
2024-03-23 22:42:26 +0600

    Refactor fetchCachedUser with optimal solution

--- backend/src/cached/user.cached.ts
+++ backend/src/cached/user.cached.ts
@@ -1,21 +1,18 @@
 import redis from '@/lib/redis';
 import { User } from '@/models/user.model';
+import mongoose from 'mongoose';
 
-export const fetchCachedUser = async (_id: string, expiretime: number = 3600) => {
-  try {
-    const redisKey = 'user:' + _id;
-    const cachedUser = await redis.get(redisKey);
-    if (cachedUser) {
-      return JSON.parse(cachedUser);
-    }
+export const fetchCachedUser = async (_id: string, expiration: number = 3600) => {
+  const redisKey = 'user:' + _id;
+  const cachedUser = await redis.get(redisKey);
+  if (cachedUser) {
+    return JSON.parse(cachedUser);
+  }
+  if (mongoose.Types.ObjectId.isValid(_id)) {
     const uncachedUser = await User.findById(_id);
     if (uncachedUser) {
-      await redis.setex(redisKey, expiretime, JSON.stringify(uncachedUser));
+      await redis.setex(redisKey, expiration, JSON.stringify(uncachedUser));
       return uncachedUser;
-    }
-  } catch (err: any) {
-    if (!String(err.message).includes('Cast to ObjectId failed for value')) {
-      throw err;
     }
   }
   return null;
